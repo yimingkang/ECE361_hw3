48456a2449466c33bda78e2602a4504eb1800b9d
Ryan Kang <yimingkang@hotmail.com>
2015-10-19 17:44:06 -0400

    completed part 2

--- Client.java
+++ Client.java
@@ -8,24 +8,19 @@ public class Client{
         Socket socket = new Socket("localhost",9876);
         Scanner scr = new Scanner(System.in);
         int nPack = scr.nextInt();
+        int pError = scr.nextInt();
         int sent = 1;
         BufferedReader socket_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
 
+        // send nPack and pError to server
         writer.writeBytes(nPack + "" + "\r\n");
+        writer.writeBytes(pError + "" + "\r\n");
 		while(sent <= nPack){
             // send the integer
             System.out.println("Client sending " + sent);
             writer.writeBytes(sent + "" + "\r\n");
-
-            // wait for response
-            String response = socket_reader.readLine();
-            if (Integer.parseInt(response) == sent) {
-                sent += 1;
-            }else{
-                System.out.println("Client didnt get expected package number, expecting: " + sent);
-            }
-
+            sent += 1;
 		}
         System.out.println("Sent all " + nPack + " packets");
         socket.close();
