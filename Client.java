bbfa6b971652279eb4705d092e2a1299a5977373
Ryan Kang <yimingkang@hotmail.com>
2015-10-23 11:03:41 -0400

    Part 5 done

--- Client.java
+++ Client.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Client{
     public static int lastAck = 0;
-    public static int nPack = 5;
+    public static int nPack = 10;
     public static int pError = 0;
-    public static int wSize = 2;
-    public static long timeOut = 5000L;
+    public static int wSize = 4;
+    //public static long timeOut = 5000L;
     public static long startTime = 0L;
 
     public static void setAck(int n){
@@ -17,68 +17,72 @@ public class Client{
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-        Socket socket = new Socket("localhost",9876);
-        Scanner scr = new Scanner(System.in);
-        System.out.print("Please enter the number of packets:");
-        // nPack = scr.nextInt();
-        System.out.print("Please enter the error rate (between 0 and 100):");
-        // pError = scr.nextInt();
-        System.out.print("Please enter the window size:");
-        // wSize = scr.nextInt();
-        System.out.print("Please enter timeout threshold (in milliseconds):");
-        // timeOut = scr.nextLong();
-        long timer[] = new long[wSize];
-        int sent = 1;
-        DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
+        long timeOut;
+        for (timeOut = 300L; timeOut < 50000L; timeOut *= 2){
+            Socket socket = new Socket("localhost",9876);
+            Scanner scr = new Scanner(System.in);
+            System.out.print("Please enter the number of packets:");
+            // nPack = scr.nextInt();
+            System.out.print("Please enter the error rate (between 0 and 100):");
+            // pError = scr.nextInt();
+            System.out.print("Please enter the window size:");
+            // wSize = scr.nextInt();
+            System.out.print("Please enter timeout threshold (in milliseconds):");
+            // timeOut = scr.nextLong();
+            long timer[] = new long[wSize];
+            int sent = 1;
+            DataOutputStream writer = new DataOutputStream(socket.getOutputStream());
 
-        // start listener
-        Thread thread = new Thread(new Listener(socket));
-        thread.start();
+            // start listener
+            Thread thread = new Thread(new Listener(socket));
+            thread.start();
 
-        // send nPack and pError to server
-        writer.write(nPack);
-        System.out.println("Client sending number of packets:" + nPack);
-        writer.write(pError);
-        System.out.println("Client sending error rate:" + pError);
-        startTime = System.currentTimeMillis();
+            // send nPack and pError to server
+            writer.write(nPack);
+            System.out.println("Client sending number of packets:" + nPack);
+            writer.write(pError);
+            System.out.println("Client sending error rate:" + pError);
+            startTime = System.currentTimeMillis();
 
-        // Send wSize packets to server
-		while(sent <= wSize){
-            System.out.println("Client sending packet: " + sent);
-            writer.write(sent);
-            sent += 1;
-            timer[(sent-1) % wSize] = System.currentTimeMillis();
-		}
-
-        // Send nPack packets to server
-		while(sent <= nPack){
-            if (sent - lastAck <= wSize){
+            // Send wSize packets to server
+            while(sent <= wSize){
                 System.out.println("Client sending packet: " + sent);
                 writer.write(sent);
-                sent +=1;
+                sent += 1;
                 timer[(sent-1) % wSize] = System.currentTimeMillis();
             }
-            if ((System.currentTimeMillis() - timer[lastAck % wSize]) > timeOut){
-                System.out.println("Time out occurs! Resend packet: "+ (lastAck+1));
-                sent = lastAck+1;
-                timer[lastAck % wSize] = System.currentTimeMillis();
-            }
-		}
 
-        System.out.println("Sent all " + nPack + " packets");
-
-        // Wait for the other thread to receive all acknowledges.
-        while (lastAck < nPack) {
-            // System.out.println("Ack is " + lastAck);
-            try {
-                Thread.sleep(1);
-            }catch (InterruptedException e) {
-                e.printStackTrace();
+            // Send nPack packets to server
+            while(sent <= nPack){
+                if (sent - lastAck <= wSize){
+                    System.out.println("Client sending packet: " + sent);
+                    writer.write(sent);
+                    sent +=1;
+                    timer[(sent-1) % wSize] = System.currentTimeMillis();
+                }
+                if ((System.currentTimeMillis() - timer[lastAck % wSize]) > timeOut){
+                    System.out.println("Time out occurs! Resend packet: "+ (lastAck+1));
+                    sent = lastAck+1;
+                    timer[lastAck % wSize] = System.currentTimeMillis();
+                }
             }
+
+            System.out.println("Sent all " + nPack + " packets");
+
+            // Wait for the other thread to receive all acknowledges.
+            while (lastAck < nPack) {
+                // System.out.println("Ack is " + lastAck);
+                try {
+                    Thread.sleep(1);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            long endTime = System.currentTimeMillis();
+            System.err.println("Timeout="+ timeOut+", transferring took " + (endTime - startTime) + " milliseconds");
+            socket.close();
+            lastAck = 0;
         }
-        long endTime = System.currentTimeMillis();
-        System.out.println("Transferring took " + (endTime - startTime) + " milliseconds");
-        socket.close();
 	}
 }
 
